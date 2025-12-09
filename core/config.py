395fc33348d1e3abf11076bb8e99e24c333a1cf2
Jebermudez <jebermudez@unal.edu.co>
2025-12-09 18:28:54 -0500

    Compose is no longer funny, added turnstile logic

--- core/config.py
+++ core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     AUTH_SERVICE_URL: str
     USER_SERVICE_URL: str
+    TURNSTILE_SECRET_KEY: str
     SECRET_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env")
